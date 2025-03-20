f2875f34359b0b23ffe306d81754c0011d05e031
borisliao <borisliaos@gmail.com>
2025-03-19 21:23:30 -0400

    Initial commit with Astro, React, and TailwindCSS

---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,3 @@
+export const HelloWorld = () => {
+  return <p className="text-red-500">Hello World</p>;
+};
---
+++ src/layouts/Index.tsx
@@ -0,0 +1,13 @@
+import { HelloWorld } from "../components/HelloWorld";
+
+/**
+ * This will be our React SPA index.tsx file.
+ */
+export const Index = () => {
+  return (
+    <>
+      <div>It's a react component</div>
+      <HelloWorld />
+    </>
+  );
+};
